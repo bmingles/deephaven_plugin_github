fc43d49a144f78d62bd664db5e939ddb75b2d68b
Brian Ingles <brianingles@deephaven.io>
2024-07-31 15:36:42 -0500

    Dependencies

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-rm -rf build dist
+rm -rf .debug build dist
 
 # Build js
 pushd src/js
@@ -7,4 +7,5 @@ popd
 
 # Build python
 python -m build --wheel
-pip install --upgrade --no-deps --force-reinstall dist/deephaven_plugin_github-0.0.1.dev0-py3-none-any.whl
+pip uninstall -y deephaven-plugin-github
+pip install dist/deephaven_plugin_github-0.0.1.dev0-py3-none-any.whl
